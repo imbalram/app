edb61ef9b4a768c8b3999b84aab976ba836db841
Balram thakur <56107244+imbalram@users.noreply.github.com>
2020-12-15 11:11:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
 from flask import Flask,render_template,request
 import smtplib
 app=Flask(__name__)
@@ -7,25 +13,18 @@ def xyz():
 @app.route("/basicdetails",methods=['GET','POST'])
 def hjfhjfhjf():
     if(request.method=='POST'):
-        
-        Name=request.form['n1']
-
+        name=request.form['n1']
         email=request.form['e1']
         phone=request.form['p1']
         course=request.form['c1']
-
         
         
         server=smtplib.SMTP_SSL('smtp.gmail.com',465)
-        server.login('faketestcodeheroku@gmail.com','balram5525')
-        TEXT='Hello {0},\nThank you for register in our  college Ims ghaziabad\nour faculty will call you for the {1} course you enrolled soon\nThank you '.format(Name,course)
-        SUBJECT = "Acknowledgement"
-        message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
-
-        server.sendmail("faketestcodeheroku@gmail.com", email, message)
+        server.login('intro.nitin@gmail.com','Nitinstudent@123')
+        msg='Hello {0},\nthank you for register in xyz college\nour faculty will call you for the {1}course\nThank you '.format(name,course)
+        server.sendmail("intro.nitin@gmail.com",email,msg)
         server.quit()
         
         return render_template("registeration.html")
-
 if __name__=="__main__":
     app.run()
